4f8af52a67a37c4b18a896d00bc1faab24eddb5c
Seanzy <sdgurson@gmail.com>
2020-11-09 21:00:19 -0800

    Simple Letter Removal Kata Codewars

---
+++ pythonpractice.py
@@ -0,0 +1,14 @@
+def solve(st,k): 
+  print('sean')
+
+
+solve('abracadabra', 1)
+ # = 'bracadabra' # remove the leftmost 'a'.
+# solve('abracadabra', 2)
+ # = 'brcadabra'  # remove 2 'a' from the left.
+# solve('abracadabra', 6)
+#  # = 'rcdbr'      # remove 5 'a', remove 1 'b' 
+# solve('abracadabra', 8)
+#  # = 'rdr'
+# solve('abracadabra',50)
+ # = ''
